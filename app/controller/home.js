00e3bd0443b1db4c29d1842f1d55efab0df01070
zhuyuzhu <struggle_zhu@163.com>
2021-07-16 16:18:39 +0800

    fix 首页内容

--- app/controller/home.js
+++ app/controller/home.js
@@ -6,7 +6,11 @@ class HomeController extends Controller {
   async index() {
     console.log(this)
     const { ctx } = this;
-    ctx.body = this; //响应内容
+    let content = `服务器上开通的端口有限，可以通过http访问的是80端口，所以通过浏览器访问的时候，
+    只能通过ip地址+80端口来访问服务器，再经过nginx的转发，跳转到172.0.0.1:7001端口上，该端口服务
+    是由egg.js启动的node服务。
+    通过在服务器上安装git，yum git，每次通过git来获取项目代码，再进行编译运行。`
+    ctx.body = content; //响应内容
   }
 }
 
